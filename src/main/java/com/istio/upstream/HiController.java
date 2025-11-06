53c7ce8529dda90aac8b959c40b438c6127748ea
Prashanth Sriram <prashanthsriram145@gmail.com>
2025-11-06 08:48:47 +0530

    Changes to implement retry of dowstream from upstream. Also, check how downstream is not getting invoked since it is not assoicated with istio-gateway.

--- src/main/java/com/istio/upstream/HiController.java
+++ src/main/java/com/istio/upstream/HiController.java
@@ -1,12 +1,24 @@
 package com.istio.upstream;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Random;
+
 @RestController
+@Slf4j
 public class HiController {
+    Random random = new Random();
+
     @GetMapping("/hi")
-    public String hi() {
-        return "Hello from Service B";
+    public ResponseEntity<String> hi() {
+        if(random.nextBoolean()) {
+            log.info("Custom internal error");
+            return ResponseEntity.internalServerError().body("Custom internal error");
+        }
+        log.info("Success response from downstream");
+        return ResponseEntity.ok("Hello from Service B");
     }
 }
